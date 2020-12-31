f8a0cf5b96b1145ef39570b0d61e6905156320f0
Emmanuel Zoolander <12421140+emmanuelcu@users.noreply.github.com>
2020-12-31 14:14:48 -0600

    The tableView is created and three elements are shown.

---
+++ Todoey/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Todoey
+//
+//  Created by Emmanuel Cuevas on 31/12/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let cellIdentifier = "ToDoItemCell"
+}
